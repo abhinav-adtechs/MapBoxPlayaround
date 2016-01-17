152ad3a5c2b037a8926cb73e065a93a74f050e99
Abhinav <abhinav.adtechs@gmail.com>
2016-01-17 12:58:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/jordiie/mapbox101/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.jordiie.mapbox101;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setCenterCoordinate(new LatLng(41.885, -87.679));
+        mapView.setZoomLevel(11);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(41.885, -87.679))
+                .title("Mark Benson")
+                .snippet("Updated 13hours ago."));
+
+        mapView.setStyleUrl(Style.DARK);
+
+    }
+}
